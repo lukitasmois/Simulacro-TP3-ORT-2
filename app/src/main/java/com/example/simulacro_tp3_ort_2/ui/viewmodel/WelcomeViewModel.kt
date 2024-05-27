ba58a1fff51622e464f2d77effc6debfddc92798
lucasmoix <129341492+lucasmoix@users.noreply.github.com>
2024-05-27 13:33:08 -0300

    cracion de la pantalla de bienvenida

---
+++ app/src/main/java/com/example/simulacro_tp3_ort_2/ui/viewmodel/WelcomeViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.simulacro_tp3_ort_2.ui.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class WelcomeViewModel @Inject constructor() : ViewModel(){
+
+    fun buttonClick() {
+        //TODO: Implementar navegacion
+    }
+}
--- app/src/main/java/com/example/simulacro_tp3_ort_2/ui/views/activities/ActivityWelcome.kt
+++ app/src/main/java/com/example/simulacro_tp3_ort_2/ui/views/activities/ActivityWelcome.kt
@@ -1,16 +1,29 @@
 package com.example.simulacro_tp3_ort_2.ui.views.activities
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.simulacro_tp3_ort_2.R
+import com.example.simulacro_tp3_ort_2.databinding.LayActivityWelcomeBinding
+import com.example.simulacro_tp3_ort_2.ui.viewmodel.WelcomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class ActivityWelcome : AppCompatActivity() {
+    lateinit var binding: LayActivityWelcomeBinding
+    private val welcomeViewModel: WelcomeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.lay_activity_welcome)
+        binding = LayActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.layWelcomeButton.setOnClickListener { welcomeViewModel.buttonClick() }
+
+
+
     }
 }
