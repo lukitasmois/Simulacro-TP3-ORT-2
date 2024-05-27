a0944cdc8261c6f33962796ae48dae559c6a55bf
lucasmoix <129341492+lucasmoix@users.noreply.github.com>
2024-05-27 14:08:54 -0300

    agregamos bottom navigation bar

--- app/src/main/java/com/example/simulacro_tp3_ort_2/ui/views/activities/ActivityHome.kt
+++ app/src/main/java/com/example/simulacro_tp3_ort_2/ui/views/activities/ActivityHome.kt
@@ -5,12 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.simulacro_tp3_ort_2.R
+import com.example.simulacro_tp3_ort_2.databinding.LayActivityHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ActivityHome : AppCompatActivity() {
+    lateinit var binding: LayActivityHomeBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = LayActivityHomeBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.lay_activity_home)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val bottomNavView = binding.activityHomeBottomBar
+        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
+
     }
 }
